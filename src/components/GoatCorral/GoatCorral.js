082868e604abfdf27c5d595c3743d7d36d961687
Gabriel Seals <sealsgj@gmail.com>
2019-12-09 21:30:58 -0600

    freeing the goat works

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -6,12 +6,14 @@ import goatShape from '../../helpers/propz/goatShape';
 class GoatCorral extends React.Component {
   static propTypes = {
     goats: PropTypes.arrayOf(goatShape.goatShape),
+    freeGoat: PropTypes.func,
   }
 
   render() {
     const myGoats = this.props.goats;
+    const { freeGoat } = this.props;
     // myGoats is the whole array. for each goat component, we are only passing one through the function
-    const goatCards = myGoats.map((goat) => <Goat key={goat.id} goat={goat} />);
+    const goatCards = myGoats.map((goat) => <Goat key={goat.id} goat={goat} freeGoat={freeGoat}/>);
 
     return (
       <div className="goatCorral row">
