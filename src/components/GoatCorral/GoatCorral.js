2728f805e43f6449cbbc440b70b1881d4f95b87b
Gabriel Seals <sealsgj@gmail.com>
2019-12-09 20:19:21 -0600

    printing to cards

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Goat from '../Goat/Goat';
 
 class GoatCorral extends React.Component {
   render() {
     const myGoats = this.props.goats;
+    // myGoats is the whole array. for each goat component, we are only passing one through the function
+    const goatCards = myGoats.map((goat) => <Goat key={goat.id} goat={goat} />);
+
     return (
-      <div className="goatCorral">
-        Goat Corral
+      <div className="goatCorral row">
+        {goatCards}
       </div>
     );
   }
